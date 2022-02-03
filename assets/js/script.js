38e4416a28f247f2ec9abadb8b90d33e403eb0c7
Jeremiah <jclanc7507@gmail.com>
2022-02-02 21:45:24 -0500

    created necessary containers in html and began working on api url

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// search button does two main things:
+// search will take the given city name, and will give default results for the state, zip code, and country
+// search will then add the necessary information to the OpenWeather API URL, creating a unique link to a specific city with it's weather information
+// search button will finally take the located data and will fill the <div day 1-5> elements on the html
+// search results will be listed both in front end and back end of web page
