e2c77145c73c6ab4a5ef3408df37c99bba7f9815
Jeremiah <jclanc7507@gmail.com>
2022-02-05 17:14:03 -0500

    pivoted project, deleted original files, merging code with css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,41 @@
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-// fields in API response:
-
-// for temp in F and wind speed in MPH, use "&units"
-
-// current.dt = current time
-// current.weather
-// current.weather.icon
-// current.temp
-// current.humidity
-// current.wind_speed
-// current.uvi
-
-// daily.dt
-// daily.weather.icon
-// daily.temp
-// daily.wind_speed
-// daily.humidity
-
-var fetchBtn = document.getElementById("fetch-btn");
-var currentCity = document.getElementById("current");
-var currentWeather = document.getElementById("current.weather current.weather.icon");
-var currentTemp = document.getElementById("current.temp");
-var currentHumid = document.getElementById("current.humidity");
-var currentWind = document.getElementById("current.wind_speed");
-var currentUvi = document.getElementById("current.uvi");
-
-
-
-//$("#fetch-button").on("click", function() {
-//    var city = $("#city").val();
-//
-//    var 
-//}
-
-var getCurrentWeather = function() {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=6c67c4835694c684558e73f604f2beb5";
-    console.log("fetched api url");
-    console.log(apiUrl);
+let weather = {
+    "apiKey": "6c67c4835694c684558e73f604f2beb5",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&exclude=minutely&units=imperial&appid=" 
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data; 
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".icon").innerText = icon;
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "mph";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function() {
+       this.fetchWeather(document.querySelector(".search-box").value);
+    }
 };
 
-getCurrentWeather();
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+
+document.querySelector(".search-box").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Chambersburg");
