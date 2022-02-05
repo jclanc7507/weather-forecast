dd6b1fcfde17ef62e422e68d424a769b9e0a756d
Jeremiah <jclanc7507@gmail.com>
2022-02-05 08:47:43 -0500

    experimenting on different html & js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
-// search button does two main things:
-// search will take the given city name, and will give default results for the state, zip code, and country
-// search will then add the necessary information to the OpenWeather API URL, creating a unique link to a specific city with it's weather information
-// search button will finally take the located data and will fill the <div day 1-5> elements on the html
-// search results will be listed both in front end and back end of web page
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// fields in API response:
+
+// for temp in F and wind speed in MPH, use "&units"
+
+// current.dt = current time
+// current.weather
+// current.weather.icon
+// current.temp
+// current.humidity
+// current.wind_speed
+// current.uvi
+
+// daily.dt
+// daily.weather.icon
+// daily.temp
+// daily.wind_speed
+// daily.humidity
+
+var fetchBtn = document.getElementById("fetch-btn");
+var currentCity = document.getElementById("current");
+var currentWeather = document.getElementById("current.weather current.weather.icon");
+var currentTemp = document.getElementById("current.temp");
+var currentHumid = document.getElementById("current.humidity");
+var currentWind = document.getElementById("current.wind_speed");
+var currentUvi = document.getElementById("current.uvi");
+
+
+
+//$("#fetch-button").on("click", function() {
+//    var city = $("#city").val();
+//
+//    var 
+//}
+
+var getCurrentWeather = function() {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=6c67c4835694c684558e73f604f2beb5";
+    console.log("fetched api url");
+    console.log(apiUrl);
+};
+
+getCurrentWeather();
